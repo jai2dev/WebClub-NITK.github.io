b784e6de8b760bd1fc58f827d8a457fc499d1794
mananpoddar <mananpoddarm@gmail.com>
2019-12-17 16:37:38 +0530

    add home component

---
+++ src/components/home.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+
+class Home extends React.Component {
+    constructor(){
+      super();
+      this.state={
+  
+      }
+    }
+    render(){
+      return (
+        <div>
+            home Component
+        </div>
+      );
+    }  
+  }
+    
+  
+  export default Home;
+  

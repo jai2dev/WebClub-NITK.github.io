c904d73f75275e82e0e6ed1dabefe68edbd6f9b2
mananpoddar <mananpoddarm@gmail.com>
2019-12-17 17:03:26 +0530

    add style in home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import '../styles/home.css';
 
 class Home extends React.Component {
     constructor(){
@@ -10,8 +10,13 @@ class Home extends React.Component {
     }
     render(){
       return (
-        <div>
-            home Component
+        <div className="row main-image center-flex">
+            <div className="col l6">
+                 <h1 className="main-title ">Playing with </h1>
+            </div>
+            <div className="col l6">
+                <img className="bg-image" width="700" src={"../assets/images/bg-002.png"} />
+            </div>
         </div>
       );
     }  

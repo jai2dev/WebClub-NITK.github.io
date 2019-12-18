912f4e91b82e948849f21ede789b9c1df7bcc376
Sriram2001 <sriramudupi19@gmail.com>
2019-12-18 19:45:02 +0530

    Added props to memberCard

--- src/components/member.js
+++ src/components/member.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../styles/member.css';
 import SigNav from './sigNav.js';
 import YearNav from './yearNav.js';
@@ -6,13 +6,32 @@ import MemberCard from './memberCard.js';
 
 
 export default function CenteredTabs() {
-
+  
+  // const members = JSON.parse('../assets/data/webclubMemberData.json');
+  // let members = require('../assets/data/webclubMemberData.json');
+  
   return (
     <div>
         <h1>Members</h1>
         <SigNav></SigNav>
         <YearNav></YearNav>
-        <MemberCard></MemberCard>
+        
+        <MemberCard 
+         name="Adarsh Naidu"
+         role="Club member"
+         email="#"
+         githuburl="#"
+         linkedinurl="#"
+         image="https://cdn.glitch.com/21943bea-5c73-4cf3-81b5-1b7fd62627ba%2Fpro.jpg?v=1576657391203"
+        />
+        {/* <MemberCard 
+         name={members[0].name}
+         role={members[0].role}
+         email="#"
+         githuburl="#"
+         linkedinurl="#"
+         image="https://cdn.glitch.com/21943bea-5c73-4cf3-81b5-1b7fd62627ba%2Fpro.jpg?v=1576657391203"
+        /> */}
     </div>
   );
 }
--- src/components/memberCard.js
+++ src/components/memberCard.js
@@ -3,17 +3,17 @@ import '../styles/memberCard.css';
 
 // function memberCard(){
 //     return(
-//         // <div class="container">
-//         // <div class="card" data-aos="flip-left">
-//         //     <div class="image">
+//         // <div className="container">
+//         // <div className="card" data-aos="flip-left">
+//         //     <div className="image">
 //         //         <img src="pro.jpg"></img>
-//         //         <div class="links">
-//         //             <span class="mdi mdi-github-circle"></span>
-//         //             <span class="mdi mdi-email"></span>
-//         //             <span class="mdi mdi-linkedin-box"></span>
+//         //         <div className="links">
+//         //             <span className="mdi mdi-github-circle"></span>
+//         //             <span className="mdi mdi-email"></span>
+//         //             <span className="mdi mdi-linkedin-box"></span>
 //         //         </div>
 //         //     </div>
-//         //     <div class="info">
+//         //     <div className="info">
 //         //         <h2>Adarsh Naidu</h2>
 //         //         <p>Club Member</p>
 //         //     </div>
@@ -22,38 +22,68 @@ import '../styles/memberCard.css';
 //         <h1>hello</h1>
 //     );
 // }
+// "https://cdn.glitch.com/21943bea-5c73-4cf3-81b5-1b7fd62627ba%2Fpro.jpg?v=1576657391203"
 
 // export default memberCard;
 
 
+// function MemberCard(props){    
+//       return (
+//         <div className="container">
+//         <div className="card" data-aos="flip-left">
+//             <div className="image">
+//                 <img src={props.image}></img>
+//                 <div className="links">
+//                     <a href={props.githuburl}><span className="mdi mdi-github-circle"></span></a>
+//                     <a href={props.email}><span className="mdi mdi-email"></span></a>
+//                     <a href={props.linkedinurl}><span className="mdi mdi-linkedin-box"></span></a>
+//                     {/* <span className="mdi mdi-github-circle"></span>
+//                     <span className="mdi mdi-email"></span>
+//                     <span className="mdi mdi-linkedin-box"></span> */}
+//                 </div>
+//             </div>
+//             <div className="info">
+//                 <h2>{props.name}</h2>
+//                 <p>{props.role}</p>
+//             </div>
+//         </div>
+//         </div>
+//       );
+// }  
+
+
+
 class MemberCard extends React.Component {
-    constructor(props){
-      super(props);
-      this.state={
-  
-      }
-    }
-    render(){
-      return (
-        <div class="container">
-        <div class="card" data-aos="flip-left">
-            <div class="image">
-                <img src="https://cdn.glitch.com/21943bea-5c73-4cf3-81b5-1b7fd62627ba%2Fpro.jpg?v=1576657391203"></img>
-                <div class="links">
-                    <span class="mdi mdi-github-circle"></span>
-                    <span class="mdi mdi-email"></span>
-                    <span class="mdi mdi-linkedin-box"></span>
+constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+}
+
+    render() {
+        return (
+            <div className="container">
+                <div className="card" data-aos="flip-left">
+                    <div className="image">
+                        <img src={this.props.image}></img>
+                        <div className="links">
+                            <a href={this.props.githuburl}><span className="mdi mdi-github-circle"></span></a>
+                            <a href={this.props.email}><span className="mdi mdi-email"></span></a>
+                            <a href={this.props.linkedinurl}><span className="mdi mdi-linkedin-box"></span></a>
+                        </div>
+                    </div>
+                <div className="info">
+                    <h2>{this.props.name}</h2>
+                    <p>{this.props.role}</p>
+                    </div>
                 </div>
             </div>
-            <div class="info">
-                <h2>Adarsh Naidu</h2>
-                <p>Club Member</p>
-            </div>
-        </div>
-        </div>
-      );
-    }  
+        );
+    }
 }
-    
+
+
+   
   
 export default MemberCard;

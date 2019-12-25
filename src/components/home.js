32a08124d47ebef2ce4fd86a15f4713085ffcc22
mananpoddar <mananpoddarm@gmail.com>
2019-12-26 00:15:22 +0530

    change colors of home component

--- src/components/home.js
+++ src/components/home.js
@@ -14,7 +14,10 @@ class Home extends React.Component {
         <Nav sticky="false" transp="true" />
         <div className="row main-image center-flex">
           <div className="col l6">
-            <h1 className="main-title ">Playing with </h1>
+            <h1> we are </h1>
+            <h1 className="main-title ">Web Enthusiasts` </h1>
+            <h1 className="main-title ">Club  </h1>
+            <h1>NITK</h1>
           </div>
           <div className="col l6">
             <img className="bg-image" width="700" src={imageUrl} />

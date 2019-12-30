49d53fa48946e390f5c6a04290bae2d25bdd07e2
AdarshNaidu <naidu.adarsh2511@gmail.com>
2019-12-31 01:05:45 +0530

    Updates

---
+++ precache-manifest.9905e78829a9829be745611eb07415be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b522bf9d2543373117dd9c565ff751",
+    "url": "/web-club-new-website/index.html"
+  },
+  {
+    "revision": "94a6ff66ad882f6cee8e",
+    "url": "/web-club-new-website/static/css/main.04a2f18c.chunk.css"
+  },
+  {
+    "revision": "6605825e0378f8841366",
+    "url": "/web-club-new-website/static/js/2.976c52ea.chunk.js"
+  },
+  {
+    "revision": "374a015f9bd7dbb4f757a46f1c6a7946",
+    "url": "/web-club-new-website/static/js/2.976c52ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94a6ff66ad882f6cee8e",
+    "url": "/web-club-new-website/static/js/main.51232ff5.chunk.js"
+  },
+  {
+    "revision": "c0ef498a322bdba5313d",
+    "url": "/web-club-new-website/static/js/runtime-main.7c511917.js"
+  },
+  {
+    "revision": "10c4b1d63df69e987a28bfca152e4213",
+    "url": "/web-club-new-website/static/media/bg-002.10c4b1d6.png"
+  },
+  {
+    "revision": "f62517f9edcb5798374490ffd43fa516",
+    "url": "/web-club-new-website/static/media/cd-icon-picture.f62517f9.svg"
+  },
+  {
+    "revision": "3de88b2a4c53e055c99e65868db59410",
+    "url": "/web-club-new-website/static/media/rocket.3de88b2a.png"
+  },
+  {
+    "revision": "f5444ece6d7f4b94bec86172acc78bc9",
+    "url": "/web-club-new-website/static/media/webclub-logo-blue.f5444ece.png"
+  }
+]);

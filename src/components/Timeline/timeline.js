b62dd64b1cd78b088d79cab44c90e0543bfd969e
AdarshNaidu <naidu.adarsh2511@gmail.com>
2019-12-31 23:27:27 +0530

    taking timeline data from json file

--- src/components/Timeline/timeline.js
+++ src/components/Timeline/timeline.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../../styles/timeline.css';
 import TimelineCard from './timelineCard';
 import Nav from '../Nav/Nav';
+import data from '../../assets/data/timeline.json';
 
 
 class Timeline extends React.Component {
@@ -16,19 +17,33 @@ class Timeline extends React.Component {
         <div>
         <Nav sticky="true" transp="false" />
         <section id="cd-timeline" className="cd-container">
+        {data.map(value => {
+          return(
             <TimelineCard
-              title={"Rubeus Game Engine"}
-              body={"A cross-platform 2D game engine written for beginner programmers to realise their ideas fast and seemlessly."}
-              date={"December, 2018"}
-              link={"home"}
+              title={value.title}
+              body={value.description}
+              date={value.date}
+              link={value.link}
               />
+          )
+        })}
+            
         </section>
         </div>
       );
     }  
 }
+
     
   
 export default Timeline;
   
+
+
+// <TimelineCard
+//               title={"Rubeus Game Engine"}
+//               body={"A cross-platform 2D game engine written for beginner programmers to realise their ideas fast and seemlessly."}
+//               date={"December, 2018"}
+//               link={"home"}
+//               />
 

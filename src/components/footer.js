c4b9cf0abf916062efaf1d2c50b62e0a2ed59f92
AdarshNaidu <naidu.adarsh2511@gmail.com>
2020-01-02 18:59:06 +0530

    footer links fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -18,12 +18,12 @@ class Footer extends React.Component {
                             <div className="footer-text">Contact Us</div>
                             <div className="footer-text-content"> NITK, Surathkal, Karnataka, India- 575025</div>
                             <div className="footer-links c-secondary">
-                                <p><i className="fa fa-envelope-o" aria-hidden="true"></i> <span className="f-link">webclub@nitk.edu.in</span></p>
+                                <p><i className="fa fa-envelope-o" aria-hidden="true"></i> <span className="f-link"><a href="mailto:webclub@nitk.edu.in">webclub@nitk.edu.in</a></span></p>
                                 {/* <p><i className="fa fa-phone" aria-hidden="true"></i> <span className="f-link">01332 284 521</span></p> */}
                             </div>
                             <div className="footer-social">
                                 <a href="https://www.facebook.com/web.club.nitk/"><i className="fa fa-facebook" aria-hidden="true"></i></a>
-                                <a href="github.com/WebClub-NITK/"><i className="fa fa-github" aria-hidden="true"></i></a>
+                                <a href="https://github.com/WebClub-NITK"><i className="fa fa-github" aria-hidden="true"></i></a>
                                 <a href="https://www.linkedin.com/school/web-enthusiasts-club-nitk/"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
                             </div>
                             </div>

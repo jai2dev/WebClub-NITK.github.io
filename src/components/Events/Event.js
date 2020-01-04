d723137e9852e96fa70c838bb33006558aed70b3
Sriram2001 <sriramudupi19@gmail.com>
2020-01-05 00:17:32 +0530

    Fixed events page animation

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -3,10 +3,17 @@ import EventCard from "./EventCard";
 import YearNavigation from "./yearNavigation.js";
 import Nav from "../Nav/Nav";
 import eventsData from "../../assets/data/events.json";
-import '../../styles/events.css';
-import Helmet from 'react-helmet';
+import "../../styles/events.css";
+import Helmet from "react-helmet";
 const Event = () => {
   console.log(eventsData);
+
+  const styles = {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around"
+  };
   return (
     <div>
       <Helmet>
@@ -18,41 +25,38 @@ const Event = () => {
           <YearNavigation />
         </div>
 
-        <div className="container">
-          <div className="row">
-            {eventsData.map(value => {
-              if(value.status == 0){
-                return (
-                  <div style={{ margin: 10 }}>
-                    <EventCard
-                      classs={"past"}
-                      title={value.title}
-                      date={value.date}
-                      time={value.time}
-                      venue={value.venue}
-                      image={value.image}
-                      description={value.description}
-                    />
-                  </div>
-                );
-              }else{
-                return (
-                  <div style={{ margin: 10 }}>
-                    <EventCard
-                      classs={"upcoming"}
-                      title={value.title}
-                      date={value.date}
-                      time={value.time}
-                      venue={value.venue}
-                      image={value.image}
-                      description={value.description}
-                    />
-                  </div>
-                );
-              }
-              
-            })}
-          </div>
+        <div className="container" style={styles}>
+          {eventsData.map(value => {
+            if (value.status == 0) {
+              return (
+                <div style={{ marginBottom: 20 }}>
+                  <EventCard
+                    classs={"past"}
+                    title={value.title}
+                    date={value.date}
+                    time={value.time}
+                    venue={value.venue}
+                    image={value.image}
+                    description={value.description}
+                  />
+                </div>
+              );
+            } else {
+              return (
+                <div style={{ marginBottom: 20 }}>
+                  <EventCard
+                    classs={"upcoming"}
+                    title={value.title}
+                    date={value.date}
+                    time={value.time}
+                    venue={value.venue}
+                    image={value.image}
+                    description={value.description}
+                  />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     </div>

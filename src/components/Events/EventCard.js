c79bc234ed9d1ea0e3b87f57f3545e843bb9d80c
Sriram Rao <sriramudupi19@gmail.com>
2020-07-21 22:03:15 +0530

    Event card style fix

--- src/components/Events/EventCard.js
+++ src/components/Events/EventCard.js
@@ -32,18 +32,20 @@ class EventCard extends React.Component {
 
   render() {
     const imageUrl = `url(${this.props.image})`;
+    // console.log(`Image url: ${imageUrl}`);
     return (
       <article className={"EventCard mix "+ this.props.classs}>
         <div className="thumb" style={{ backgroundImage: imageUrl }} />
         <div className="infos">
           <h2 className="title">
-            {this.props.title}
+            <span className="ellipss">{this.props.title}</span>
+            
             <span className="time">{this.props.time}</span>
           </h2>
           <h3 className="detail">{this.props.date}</h3>
           <h3 className="detail"> {this.props.venue}</h3>
           <p className="txt">{this.props.description}</p>
-          <h3 className="details">event details</h3>
+          {/* <h3 className="details">event details</h3> */}
         </div>
       </article>
     );

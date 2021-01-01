bf66259be75364c4406775ba924d7e6aac12de1e
Sriram2001 <sriramudupi19@gmail.com>
2021-01-01 16:54:52 +0530

    Disable homepage carousel

--- src/components/upcomingEvents.js
+++ src/components/upcomingEvents.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "../styles/upcomingEvents.css";
 import EventCard from "./Events/EventCard.js";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
 import { eventsWorkSheetId } from "./../environment";
 import SpreadSheetApi from "./../_services/spreadSheetApi";
 
@@ -22,64 +22,84 @@ class UpcomingEvents extends React.Component {
     this.setState({ eventsData: finalArray });
   }
   render() {
-    const responsive = {
-      desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 3,
-        slidesToSlide: 3, // optional, default to 1.
-      },
-      tablet: {
-        breakpoint: { max: 1024, min: 464 },
-        items: 2,
-        slidesToSlide: 2, // optional, default to 1.
-      },
-      mobile: {
-        breakpoint: { max: 464, min: 0 },
-        items: 1,
-        slidesToSlide: 1, // optional, default to 1.
-      },
-    };
     return (
       <div id="home-section">
         <h1 className="heading">Upcoming Events</h1>
-
-        <div className="container upcoming-events-container">
-          <Carousel
-            swipeable={true}
-            draggable={true}
-            showDots={true}
-            responsive={responsive}
-            ssr={true} // means to render carousel on server-side.
-            infinite={true}
-            autoPlay={this.props.deviceType !== "mobile" ? true : false}
-            autoPlaySpeed={1000}
-            keyBoardControl={true}
-            customTransition="all .5"
-            transitionDuration={500}
-            containerClass="carousel-container"
-            removeArrowOnDeviceType={["tablet", "mobile"]}
-            deviceType={this.props.deviceType}
-            dotListClass="custom-dot-list-style"
-            itemClass="carousel-item-padding-40-px"
-          >
-            {this.state.eventsData.map((value) => {
-              return (
-                <div style={{ margin: 100, marginLeft: 35 }} key={value.title}>
-                  <EventCard
-                    title={value.title}
-                    date={value.date}
-                    time={value.time}
-                    venue={value.venue}
-                    image={value.image}
-                    description={value.description}
-                  />
-                </div>
-              );
-            })}
-          </Carousel>
+        <div className="container d-flex justify-content-around flex-wrap">
+          {this.state.eventsData.map(value =>
+            <EventCard
+              classs={"upcoming"}
+              title={value.title}
+              date={value.date}
+              time={value.time}
+              venue={value.venue}
+              image={value.image}
+              description={value.description}
+              extras={value.extras}
+              key={value.title}
+            />
+          )}
         </div>
       </div>
-    );
+    )
+    // const responsive = {
+    //   desktop: {
+    //     breakpoint: { max: 3000, min: 1024 },
+    //     items: 3,
+    //     slidesToSlide: 3, // optional, default to 1.
+    //   },
+    //   tablet: {
+    //     breakpoint: { max: 1024, min: 464 },
+    //     items: 2,
+    //     slidesToSlide: 2, // optional, default to 1.
+    //   },
+    //   mobile: {
+    //     breakpoint: { max: 464, min: 0 },
+    //     items: 1,
+    //     slidesToSlide: 1, // optional, default to 1.
+    //   },
+    // };
+    // return (
+    //   <div id="home-section">
+    //     <h1 className="heading">Upcoming Events</h1>
+
+    //     <div className="container upcoming-events-container">
+    //       <Carousel
+    //         swipeable={true}
+    //         draggable={true}
+    //         showDots={true}
+    //         responsive={responsive}
+    //         ssr={true} // means to render carousel on server-side.
+    //         infinite={true}
+    //         autoPlay={this.props.deviceType !== "mobile" ? true : false}
+    //         autoPlaySpeed={1000}
+    //         keyBoardControl={true}
+    //         customTransition="all .5"
+    //         transitionDuration={500}
+    //         containerClass="carousel-container"
+    //         removeArrowOnDeviceType={["tablet", "mobile"]}
+    //         deviceType={this.props.deviceType}
+    //         dotListClass="custom-dot-list-style"
+    //         itemClass="carousel-item-padding-40-px"
+    //       >
+    //         {this.state.eventsData.map((value) => {
+    //           return (
+    //             <div style={{ margin: 100, marginLeft: 35 }} key={value.title}>
+    //               <EventCard
+    //                 title={value.title}
+    //                 date={value.date}
+    //                 time={value.time}
+    //                 venue={value.venue}
+    //                 image={value.image}
+    //                 description={value.description}
+    //               />
+    //             </div>
+    //           );
+    //         })}
+    //       </Carousel>
+    //     </div>
+    //   </div>
+    // );
   }
 }
 
